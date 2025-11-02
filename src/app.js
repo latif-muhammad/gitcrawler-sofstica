dd548b33b66d36cd34825acee8fcb96c4b3897e2
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2025-11-02 17:46:43 +0500

    Final 100k repos

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { fetchRepositories } from "./crawler/crawler.js";
 const app = async () => {
   try {
     await setupDB();
-    const repos = await fetchRepositories(2000, 100);
+    const repos = await fetchRepositories(100000, 100);
     console.log("\nTop Repositories:");
     console.log(repos);
     await client.end();
